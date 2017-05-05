95fc036c60988b59d9180d43533fb52e5bd0b2c6
yk_marble <milkcocoapple@gmail.com>
2017-05-05 19:16:36 +0900

    Rewrite utils.show_image() because of slow display.

--- utils.py
+++ utils.py
@@ -4,6 +4,7 @@ import sys
 import struct
 import numpy
 import pylab
+import cv2
 
 def load_rawimage(path):
     with open(path, "rb") as f:
@@ -16,7 +17,6 @@ def load_rawimage(path):
     img.resize(height, width)
     return img
 
-
 def save_rawimage(img, outpath):
     h, w = img.shape
     header = struct.pack("ccxxII", b"P", b"0", w, h)
@@ -25,9 +25,15 @@ def save_rawimage(img, outpath):
         f.write(header)
         f.write(payload)
 
-def show_image(img):
-    pylab.imshow(img, "gray")
-    pylab.xticks(())
-    pylab.yticks(())
-    pylab.colorbar()
-    pylab.show()
+def show_image(img, clim=None):
+    """
+    @img: 2d or 3d numpy array
+    @clim: tuple contains pair of color bound, or None which means deriving them from array
+    """
+    if clim is None:
+        clim = (numpy.min(img), numpy.max(img))
+    normalized = img.copy()
+    normalized -= clim[0]
+    normalized /= clim[1] - clim[0]
+    cv2.imshow("ctpy", normalized)
+    cv2.waitKey(0)
